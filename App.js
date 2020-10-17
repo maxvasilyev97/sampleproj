2ba963e9f7e8160242844667c50a5f528b79b82e
maxvasilyev97 <maxvasilyev1997@gmail.com>
2020-10-17 11:00:03 +0300

    кнопка, стэйты, инпуты

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Button, PickerIOSComponent, StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const [name,setName] = useState('имя')
+  const [age,setAge] = useState('30')
+  const [person,setPerson] = useState({name: 'кек', age: 30})
+  
+  const clickHandler = () => {
+    setName('имя2');
+    setPerson({name: 'сас', age: 20})
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text>Введи имя</Text>
+        <TextInput style={styles.input}/>
+        <Text>Введи возраст</Text>
+        <TextInput style={styles.input}/>
+        <Text style={styles.boldText}>Имя: {name}, возраст: {age}</Text>
+      </View>
+      <View style={styles.body}>
+        <Text>{name}</Text>
+        <Text>{person.name}, {person.age}</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title='жми' onPress={clickHandler}/>
+      </View>
     </View>
   );
 }
@@ -18,4 +39,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header:{
+    backgroundColor: 'pink',
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  body: {
+    backgroundColor: 'yellow',
+    padding: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10
+  }
 });
