91d7a7da311acf9c24fb651dd3da568059589908
maxvasilyev97 <maxvasilyev1997@gmail.com>
2020-10-17 21:46:56 +0300

    импорт вьюхи

--- Views/login.js
+++ Views/login.js
@@ -1,5 +1,8 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {StyleSheet, View, Text, TextInput, Image, TouchableOpacity} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 export default function Login() {  
     return(
