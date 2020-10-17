57020ee34afca9d7c921bf0a2a42e5655781b4e7
maxvasilyev97 <maxvasilyev1997@gmail.com>
2020-10-17 20:10:23 +0300

    образец переходов

--- App.js
+++ App.js
@@ -1,12 +1,78 @@
-import React from 'react';
+import 'react-native-gesture-handler';
+import * as React from 'react';
 import Login from './Views/login';
 import Qr from './Views/qr';
 import IdForm from './Views/idfrom';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {  
+// export default function App() {  
+//   return (
+//     // <Login /> //Окно входа
+//     // <Qr /> //QR
+//     <IdForm /> //Ввод номера    
+//   );
+// }
+
+function HomeScreen({ navigation }) {
   return (
-    // <Login /> //Окно входа
-    // <Qr /> //QR
-    <IdForm /> //Ввод номера    
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => {
+          /* 1. Navigate to the Details route with params */
+          navigation.navigate('Details', {
+            itemId: 86,
+            otherParam: 'anything you want here',
+          });
+        }}
+      />
+    </View>
   );
 }
+
+function DetailsScreen({ route, navigation }) {
+  /* 2. Get the param */
+  const { itemId , otherParam} = route.params;
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() =>
+          navigation.push('Details', {
+            itemId: Math.floor(Math.random() * 100),
+          })
+        }
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ title: 'Overview' }}
+      />
+      <Stack.Screen
+        name="Details"
+        component={DetailsScreen}
+        initialParams={{ itemId: 42 }}
+      />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
